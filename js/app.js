1fbfb87362504dd92fb5c2ab6644431387eb289d
akempel <anna.kb.kempel@gmail.com>
2020-02-11 20:54:46 -0800

    Preliminary 'place your ships' with ships with a length of 3 function complete

--- js/app.js
+++ js/app.js
@@ -1,58 +1,3 @@
-console.log('sanity check');
-
-// create a game board
-// hide ships in game board
-// add click event to table
-// when clicked, check if square has ship
-// if square has ship, turn square red
-// if square does not have ship, turn square grey
-// when square is selected, tries remaining goes down
-// when ship is hit, ships sunk goes up and ships left to find goes down
-// when all ships sunk OR all tries are used, game ends
-// when game is over
-// message (and score) should be displayed (win or loss)
-// no more clicks on board should be accepted
-// reset button resets entire game ie:
-// game board and hidden ships are reshuffled
-// game board circles/ui go back to neutral (no red or gray dots)
-// sunk ships, ships left, and tries left should return to initialized values
-// sunk ships = 0
-// tries left = 24
-// ships left = num of ships i decide to shuffle into the board
-
-
-// handle click events on table
-// check if board index is equal to whatever value i want to store ships as (1 for testing)
-// if the index is equal to 1, color the div red and set board[index] equal to -1
-// if the index is null, color div gray and set board[index] equal to -1 (counts as being clicked)
-// else if -1, return nothing as its already been clicked
-
-
-// time to set initial variables for everything except the board (will stay null for  now)
-// boatsSunk = 0
-// boatsLeft = 7
-// cannonballs = 24;
-
-
-// checking for win/loss
-// game ends if
-// cannonballs === 0
-// shipsLeft === 0
-// if shipsLeft === 0, win
-// if cannonballs === 0 && shipsLeft > 0, lose
-// when game is over
-// show message ('you won!' or 'you lost')
-// disable click event. reenable with reset button.
-
-
-// time to randomize ships amongst the board!
-// first create board array with 64 characters
-// for loop, i < shipsLeft
-// board[i] = 1
-// first 7 characters in array are now ships
-// need to shuffle array to randomly place ships
-
-
 // STATIC VARIABLES
 let board;
 let shipsSunk;
@@ -111,13 +56,15 @@ function initialize() {
     cannonballs = 24;
     message.innerText = `Search for ships!`;
 
-    // TODO: randomize board
+    /*
+    // randomize board
     board = Array(64).fill(0);
     for (let i = 0; i < shipsLeft; i++) {
         board[i] = 1;
     }
     shuffle(board);
-    console.log(board);
+    */
+    board = new Array(64).fill(0);
 
     // Remove all divs in table.
     let markedSquares = document.querySelectorAll('div .map-mark');
@@ -126,12 +73,13 @@ function initialize() {
     }
 
     render();
-    table.addEventListener('click', handleGuess);
+    table.addEventListener('click', selectShips);
 }
 
+/*
 function handleGuess(event) {
 
-    let index = -1;
+    let index;
     for (let i = 0; i < tableData.length; i++) {
         if (event.target === tableData[i]) {
             index = i;
@@ -160,5 +108,103 @@ function handleGuess(event) {
     }
     render();
 }
+*/
 
 initialize();
+
+// to create/set battleships larger than 1 block
+// no random generation. board starts out all 0
+// function that runs on table click
+// player selects starting block
+// block board value = 1
+// starting block stored in variable
+// player selects surrounding blocks
+// if 2nd selected block has index +1, -1, +8, OR -8 to the original, click event runs
+// then next blocks must ALL be in the same vein of +1, -1, +8, or -8 from the previous
+// if 2nd block is +1 to starting block, 3rd block must be +1 to 2nd block or -1 to starting block
+// if 2nd block is +8 to starting block, 3rd block must be +8 to 2nd block or -8 to starting block, etc etc
+
+// starting block variable is outside the function so it is not overwritten each time the function is run
+// starting block will have an initial value
+// when square is clicked, if starting block has initial value, initial value now equals current index
+// when ship placing is complete, startingBlock returns to initial value (-1)
+
+// blocksUsed will be used to count how many blocks have been and can continue to be used to form the ship
+// each time selectShips runs successfully, blocksUsed++
+// once blocksUsed === 3 (or once it === 2. after the third block is used, it might add 1 to blocksUsed but not check if its equal to 3 until next the function runs),
+// startingBlock's value will be reset to -1
+// blocksUsed value will be reset to 0
+
+let startingIndex = -1;
+let secondIndex = -1;
+//let blocksUsed = 0;
+
+function selectShips(event) {
+    // finds index of square clicked
+    let index;
+    for (let i = 0; i < tableData.length; i++) {
+        if (event.target === tableData[i]) {
+            index = i;
+            break;
+        }
+    }
+
+    if (board[index] === 0) {
+        if (startingIndex === -1) {
+            startingIndex = index;
+        } else if (secondIndex === -1) {
+            secondIndex = index;
+        }
+    }
+
+    // if its the first click and the space is free, create first mark of ship
+    if (index === startingIndex && board[index] === 0) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'map-mark');
+        tableData[index].appendChild(div);
+        div.style.backgroundColor = 'red';
+
+        board[index] = 1;
+        console.log(startingIndex);
+
+        // if on second click and space is free, AND space is +1, -1, +8, or -8 spaces away from startingIndex, create second mark
+    } else if (index === secondIndex && board[index] === 0) {
+        if ([startingIndex - 1, startingIndex + 1, startingIndex - 8, startingIndex + 8].indexOf(index) > -1) {
+
+            let div = document.createElement('div');
+            div.setAttribute('class', 'map-mark');
+            tableData[index].appendChild(div);
+            div.style.backgroundColor = 'red';
+
+            board[index] = 1;
+            console.log(startingIndex, secondIndex);
+        }
+        // on third click and space is free, AND space is + or - 1 spaces away from secondIndex, create third mark
+    } else if (index != startingIndex && index != secondIndex && board[index] === 0) {
+        console.log(startingIndex, secondIndex);
+        if (
+            ((index === secondIndex - 1 || index === secondIndex + 1) && (secondIndex === startingIndex - 1 || secondIndex === startingIndex + 1)) ||
+            ((index === secondIndex - 8 || index === secondIndex + 8) && (secondIndex === startingIndex - 8 || secondIndex === startingIndex + 8))
+        ) {
+
+            let div = document.createElement('div');
+            div.setAttribute('class', 'map-mark');
+            tableData[index].appendChild(div);
+            div.style.backgroundColor = 'red';
+
+            board[index] = 1;
+            startingIndex = -1;
+            secondIndex = -1;
+
+        }
+    }
+
+    /*
+    if (board[index] === 0) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'map-mark');
+        tableData[index].appendChild(div);
+        div.style.backgroundColor = 'red';
+    }
+    */
+}
